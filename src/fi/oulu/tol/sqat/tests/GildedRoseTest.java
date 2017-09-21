9173bee00cb89efe91400a9ee0e40951239806ec
jnyyssol <jnyyssol@STUPID-PC727.student.yo.oulu.fi>
2017-09-21 14:59:51 +0300

    Assignment 2

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -36,7 +36,138 @@ public class GildedRoseTest {
 	}
     
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_dexVest_sellin_10_9() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("+5 Dexterity Vest", 10, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item dexVest = items.get(0);
+		assertEquals(9, dexVest.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_dexVest_quality_20_19() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("+5 Dexterity Vest", 10, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item dexVest = items.get(0);
+		assertEquals(19, dexVest.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_AgedBrie_Quality_0_1() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 2, 0) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(1, itemBrie.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_AgedBrie_sellin_2_1() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 2, 0) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(1, itemBrie.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_mongoose_sellin_5_4() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Elixir of the Mongoose", 5, 7) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item monElix = items.get(0);
+		assertEquals(4, monElix.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_mongoose_quality_7_6() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Elixir of the Mongoose", 5, 7) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item monElix = items.get(0);
+		assertEquals(6, monElix.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_Sulfuras_unchanged() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item sulfuras = items.get(0);
+		assertEquals(80, sulfuras.getQuality());
+		assertEquals(0, sulfuras.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_backstage_quality_20_21() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item backstage = items.get(0);
+		assertEquals(21, backstage.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_backstage_sellin_15_14() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item backstage = items.get(0);
+		assertEquals(14, backstage.getSellIn());
 	}
 }
